33691e5c10856ae152992c53bd56f2906d1ee98f
Damian Czerwinsky <81160264+Damianchii@users.noreply.github.com>
2023-05-24 22:36:11 +0200

    Add files via upload

--- Food.jsx
+++ Food.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
+import React, { useState, version } from "react"
 import { data } from "../data/data.js"
-import { Modal } from "../components/Modal.jsx"
+import { Modal } from "./Modal.jsx"
 
 export const Food = () => {
 	const [foods, setFoods] = useState(data)
@@ -8,6 +8,16 @@ export const Food = () => {
 	//Modal
 	const [modal, setModal] = useState(false)
 	const toggleModal = () => setModal(!modal)
+	const [foodName, setFoodName] = useState("")
+	const [foodImage, setFoodImage] = useState("")
+	const [foodDescription, setFoodDescription] = useState("")
+
+	const showItemModal = item => {
+		setModal(!modal)
+		setFoodName(item.name)
+		setFoodImage(item.image)
+		setFoodDescription(item.description)
+	}
 
 	if (modal) {
 		document.body.classList.add("overflow-y-hidden", "pr-[21px]")
@@ -34,7 +44,7 @@ export const Food = () => {
 
 	return (
 		<div className='max-w-[1640px] m-auto px-4 py-12'>
-			<h1 className='text-orange-600 text-4xl text-center'>
+			<h1 className='text-blue-600 text-4xl text-center'>
 				Top Rated Menu Items
 			</h1>
 			{/*Filter Row*/}
@@ -45,27 +55,27 @@ export const Food = () => {
 					<div className='flex justify-start flex-wrap'>
 						<button
 							onClick={() => setFoods(data)}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1 '>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1 '>
 							All
 						</button>
 						<button
 							onClick={() => filterType("burger")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							Burgers
 						</button>
 						<button
 							onClick={() => filterType("pizza")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							Pizza
 						</button>
 						<button
 							onClick={() => filterType("salad")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							Salads
 						</button>
 						<button
 							onClick={() => filterType("chicken")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							Chicken
 						</button>
 					</div>
@@ -76,27 +86,27 @@ export const Food = () => {
 					<div className='flex justify-between max-w-[390px] w-full'>
 						<button
 							onClick={() => setFoods(data)}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							All
 						</button>
 						<button
 							onClick={() => filterPrice("$")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							$
 						</button>
 						<button
 							onClick={() => filterPrice("$$")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							$$
 						</button>
 						<button
 							onClick={() => filterPrice("$$$")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							$$$
 						</button>
 						<button
 							onClick={() => filterPrice("$$$$")}
-							className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>
+							className='border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white m-1'>
 							$$$$
 						</button>
 					</div>
@@ -105,26 +115,35 @@ export const Food = () => {
 
 			{/*Display food*/}
 			<div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4 '>
-				<Modal toggleModal={toggleModal} modal={modal} />
+				{modal && (
+					<Modal
+						name={foodName}
+						image={foodImage}
+						toggleModal={toggleModal}
+						description={foodDescription}
+					/>
+				)}
 				{foods.map((item, index) => (
-					<div
-						onClick={toggleModal}
-						key={index}
-						className='border rounded-lg shadow-lg duration-300 cursor-pointer hover:shadow-2xl}'>
-						<img
-							src={item.image}
-							alt={item.name}
-							className='w-full h-[200px]  object-cover rounded-t-lg pointer-events-none'
-						/>
-						<div className='flex justify-between px-2 py-4'>
-							<p className='font-bold'>{item.name}</p>
-							<p>
-								<span className='bg-orange-500 text-white p-1 rounded-full'>
-									{item.price}
-								</span>
-							</p>
+					<>
+						<div
+							onClick={() => showItemModal(item)}
+							key={index}
+							className='border rounded-lg shadow-lg duration-300 cursor-pointer hover:shadow-2xl}'>
+							<img
+								src={item.image}
+								alt={item.name}
+								className='w-full h-[200px]  object-cover rounded-t-lg pointer-events-none'
+							/>
+							<div className='flex justify-between px-2 py-4'>
+								<p className='font-bold'>{item.name}</p>
+								<p>
+									<span className='bg-blue-500 text-white py-2 px-3 rounded-full'>
+										{item.price}
+									</span>
+								</p>
+							</div>
 						</div>
-					</div>
+					</>
 				))}
 			</div>
 		</div>
---
+++ data.js
@@ -0,0 +1,213 @@
+export const data = [
+	{
+		id: 1,
+		name: "Double Cheeseburger",
+		category: "burger",
+		image:
+			"https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+		price: "$$$$",
+		description:
+			"A cheeseburger is a classic American culinary delight, loved by people of all ages. The star of the cheeseburger is the perfectly grilled beef patty, cooked to juicy perfection. A generous slice of melted cheese, typically American or cheddar, adds a rich and creamy element to the burger. The bun, often toasted, provides a soft and slightly crispy base for the delicious toppings.",
+	},
+	{
+		id: 2,
+		name: "Bacon Cheeseburger",
+		category: "burger",
+		image:
+			"https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
+		price: "$",
+		description:
+			"The bacon cheeseburger is an iconic and mouthwatering American classic that combines the rich and savory flavors of bacon, cheese, and juicy ground beef. It's a beloved creation that has become a staple in the world of burgers and a popular choice for burger enthusiasts worldwide. At its core, a bacon cheeseburger typically consists of a grilled or charbroiled beef patty nestled between two halves of a soft and toasted bun.",
+	},
+	{
+		id: 3,
+		name: "Mushroom Burger",
+		category: "burger",
+		image:
+			"https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
+		price: "$$",
+		description:
+			"The mushroom burger is a delicious and satisfying alternative to traditional meat-based burgers, offering a flavorful and wholesome option for vegetarians and mushroom enthusiasts alike. It showcases the natural and earthy taste of mushrooms while incorporating various complementary ingredients to create a delightful culinary experience.",
+	},
+	{
+		id: 4,
+		name: "Loaded Burger",
+		category: "burger",
+		image:
+			"https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+		price: "$$$",
+		description:
+			"The loaded burger is a true indulgence that takes the classic hamburger to a whole new level of flavor and satisfaction. Packed with a mouthwatering combination of ingredients, this burger is a feast for both the eyes and the taste buds. Starting with a juicy beef patty, the loaded burger often features high-quality ground beef seasoned with a blend of spices and cooked to perfection.",
+	},
+	{
+		id: 5,
+		name: "Feta & Spinnach",
+		category: "pizza",
+		image:
+			"https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$$",
+		description:
+			"The combination of feta cheese and spinach is a classic pairing that brings a unique and delightful twist to many dishes, including burgers. When incorporated into a loaded burger, feta and spinach add a delicious and nutritious element to the overall flavor profile. ",
+	},
+	{
+		id: 6,
+		name: "Supreme Pizza",
+		category: "pizza",
+		image:
+			"https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$$$",
+		description:
+			"Supreme pizza is a mouthwatering and popular variety of pizza that is beloved for its abundant and diverse toppings. It is a flavorful medley of ingredients that delivers a satisfying and indulgent experience for pizza enthusiasts. ",
+	},
+	{
+		id: 7,
+		name: "Meat Lovers",
+		category: "pizza",
+		image:
+			"https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+		price: "$$$$",
+		description:
+			"Meat Lovers pizza is a savory and indulgent pizza variety that caters to those who have an insatiable love for meat. As the name suggests, this pizza is loaded with a variety of meats, offering a carnivorous feast of flavors. The foundation of a Meat Lovers pizza is typically a thin or thick crust that provides the base for the toppings.",
+	},
+	{
+		id: 8,
+		name: "Cheese Pizza",
+		category: "pizza",
+		image:
+			"https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$",
+		description:
+			"Cheese pizza is a timeless classic that showcases the simplicity and pure enjoyment of a well-made pizza. As the name suggests, cheese is the star of this pizza, with its gooey and melty goodness providing a rich and savory base for the toppings.",
+	},
+	{
+		id: 9,
+		name: "Kale Salad",
+		category: "salad",
+		image:
+			"https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$$",
+		description:
+			"Kale salad is a refreshing and nutritious dish that highlights the vibrant and hearty qualities of kale, a leafy green vegetable. It is a popular choice for those seeking a healthy and flavorful salad option. The key component of a kale salad is, of course, fresh kale leaves. ",
+	},
+	{
+		id: 10,
+		name: "Ceasar Salad",
+		category: "salad",
+		image:
+			"https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$$$",
+		description:
+			"Caesar salad is a classic and widely popular dish that originated in the United States but has become a staple in many parts of the world. It is a refreshing and flavorful salad known for its combination of crisp romaine lettuce, tangy dressing, savory croutons, and grated Parmesan cheese.",
+	},
+	{
+		id: 11,
+		name: "Loaded Salad",
+		category: "salad",
+		image:
+			"https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$$$$",
+		description:
+			"A loaded salad is a hearty and satisfying dish that combines a variety of flavorful ingredients to create a well-rounded and nutritious meal. It goes beyond the traditional notion of a simple side salad by incorporating an abundance of toppings and textures.",
+	},
+	{
+		id: 12,
+		name: "Fruit Salad",
+		category: "salad",
+		image:
+			"https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+		price: "$",
+		description:
+			"Fruit salad is a refreshing and vibrant dish that showcases a delightful assortment of fresh fruits. It is a popular choice for those seeking a light and healthy option, especially during warmer seasons or as a refreshing dessert. The key element of a fruit salad is, of course, the fruits themselves. A variety of colorful and ripe fruits are selected and combined to create a visually appealing and flavorful mix. ",
+	},
+	{
+		id: 13,
+		name: "Wings",
+		category: "chicken",
+		image:
+			"https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$$",
+		description:
+			"Wings, also known as chicken wings, are a beloved and popular dish that is often enjoyed as a finger food or appetizer. They are known for their crispy and flavorful exterior and tender meat, making them a crowd-pleasing option for casual gatherings, sports events, or as a delicious snack.",
+	},
+	{
+		id: 14,
+		name: "Baked Chicken",
+		category: "chicken",
+		image:
+			"https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+		price: "$$$$",
+		description:
+			"Baked chicken is a versatile and healthy dish that is popular for its simplicity and flavorful results. It involves cooking chicken pieces or a whole chicken in the oven, resulting in tender and juicy meat with a deliciously seasoned exterior.",
+	},
+	{
+		id: 15,
+		name: "Chicken Tenders",
+		category: "chicken",
+		image:
+			"https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+		price: "$",
+		description:
+			"Chicken tenders, also known as chicken fingers or chicken strips, are a popular and versatile dish that consists of boneless and skinless chicken breast meat cut into long, thin strips. They are a beloved choice for both kids and adults due to their tender texture and the ability to customize the flavors ",
+	},
+	{
+		id: 16,
+		name: "Chicken Kabob",
+		category: "chicken",
+		image:
+			"https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+		price: "$$$",
+		description:
+			"Chicken kabobs, also known as chicken kebabs, are a delicious and versatile dish consisting of skewered pieces of marinated chicken cooked over a grill or in the oven. They are a popular choice for outdoor barbecues, picnics, or as a flavorful main course.  To prepare chicken kabobs, boneless and skinless chicken breast or thigh meat is cut into bite-sized pieces and marinated to infuse it with flavor and tenderness. ",
+	},
+]
+
+export const categories = [
+	{
+		id: 1,
+		name: "Fast Food",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/FastFood_BrowseHome@3x.png",
+	},
+	{
+		id: 2,
+		name: "Pizza",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Pizza_BrowseHome@3x.png",
+	},
+	{
+		id: 3,
+		name: "Wings",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Wings_BrowseHome@3x.png",
+	},
+	{
+		id: 4,
+		name: "Indian",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Indian_BrowseHome@3x.png",
+	},
+	{
+		id: 5,
+		name: "Latest Deals",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png",
+	},
+	{
+		id: 6,
+		name: "Restaurant Rewards",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png",
+	},
+	{
+		id: 7,
+		name: "Best Overall",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png",
+	},
+	{
+		id: 8,
+		name: "Shipped Free",
+		image:
+			"https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png",
+	},
+]
