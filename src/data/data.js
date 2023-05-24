afb8a92216f06d959806c571125a3e2277eab9b4
Damian Czerwinsky <81160264+Damianchii@users.noreply.github.com>
2023-05-24 22:38:51 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -6,6 +6,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
 		price: "$$$$",
+		description:
+			"A cheeseburger is a classic American culinary delight, loved by people of all ages. The star of the cheeseburger is the perfectly grilled beef patty, cooked to juicy perfection. A generous slice of melted cheese, typically American or cheddar, adds a rich and creamy element to the burger. The bun, often toasted, provides a soft and slightly crispy base for the delicious toppings.",
 	},
 	{
 		id: 2,
@@ -14,6 +16,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
 		price: "$",
+		description:
+			"The bacon cheeseburger is an iconic and mouthwatering American classic that combines the rich and savory flavors of bacon, cheese, and juicy ground beef. It's a beloved creation that has become a staple in the world of burgers and a popular choice for burger enthusiasts worldwide. At its core, a bacon cheeseburger typically consists of a grilled or charbroiled beef patty nestled between two halves of a soft and toasted bun.",
 	},
 	{
 		id: 3,
@@ -22,6 +26,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
 		price: "$$",
+		description:
+			"The mushroom burger is a delicious and satisfying alternative to traditional meat-based burgers, offering a flavorful and wholesome option for vegetarians and mushroom enthusiasts alike. It showcases the natural and earthy taste of mushrooms while incorporating various complementary ingredients to create a delightful culinary experience.",
 	},
 	{
 		id: 4,
@@ -30,6 +36,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
 		price: "$$$",
+		description:
+			"The loaded burger is a true indulgence that takes the classic hamburger to a whole new level of flavor and satisfaction. Packed with a mouthwatering combination of ingredients, this burger is a feast for both the eyes and the taste buds. Starting with a juicy beef patty, the loaded burger often features high-quality ground beef seasoned with a blend of spices and cooked to perfection.",
 	},
 	{
 		id: 5,
@@ -38,6 +46,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$$",
+		description:
+			"The combination of feta cheese and spinach is a classic pairing that brings a unique and delightful twist to many dishes, including burgers. When incorporated into a loaded burger, feta and spinach add a delicious and nutritious element to the overall flavor profile. ",
 	},
 	{
 		id: 6,
@@ -46,6 +56,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$$$",
+		description:
+			"Supreme pizza is a mouthwatering and popular variety of pizza that is beloved for its abundant and diverse toppings. It is a flavorful medley of ingredients that delivers a satisfying and indulgent experience for pizza enthusiasts. ",
 	},
 	{
 		id: 7,
@@ -54,6 +66,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
 		price: "$$$$",
+		description:
+			"Meat Lovers pizza is a savory and indulgent pizza variety that caters to those who have an insatiable love for meat. As the name suggests, this pizza is loaded with a variety of meats, offering a carnivorous feast of flavors. The foundation of a Meat Lovers pizza is typically a thin or thick crust that provides the base for the toppings.",
 	},
 	{
 		id: 8,
@@ -62,6 +76,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$",
+		description:
+			"Cheese pizza is a timeless classic that showcases the simplicity and pure enjoyment of a well-made pizza. As the name suggests, cheese is the star of this pizza, with its gooey and melty goodness providing a rich and savory base for the toppings.",
 	},
 	{
 		id: 9,
@@ -70,6 +86,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$$",
+		description:
+			"Kale salad is a refreshing and nutritious dish that highlights the vibrant and hearty qualities of kale, a leafy green vegetable. It is a popular choice for those seeking a healthy and flavorful salad option. The key component of a kale salad is, of course, fresh kale leaves. ",
 	},
 	{
 		id: 10,
@@ -78,6 +96,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$$$",
+		description:
+			"Caesar salad is a classic and widely popular dish that originated in the United States but has become a staple in many parts of the world. It is a refreshing and flavorful salad known for its combination of crisp romaine lettuce, tangy dressing, savory croutons, and grated Parmesan cheese.",
 	},
 	{
 		id: 11,
@@ -86,6 +106,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$$$$",
+		description:
+			"A loaded salad is a hearty and satisfying dish that combines a variety of flavorful ingredients to create a well-rounded and nutritious meal. It goes beyond the traditional notion of a simple side salad by incorporating an abundance of toppings and textures.",
 	},
 	{
 		id: 12,
@@ -94,6 +116,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
 		price: "$",
+		description:
+			"Fruit salad is a refreshing and vibrant dish that showcases a delightful assortment of fresh fruits. It is a popular choice for those seeking a light and healthy option, especially during warmer seasons or as a refreshing dessert. The key element of a fruit salad is, of course, the fruits themselves. A variety of colorful and ripe fruits are selected and combined to create a visually appealing and flavorful mix. ",
 	},
 	{
 		id: 13,
@@ -102,6 +126,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$$",
+		description:
+			"Wings, also known as chicken wings, are a beloved and popular dish that is often enjoyed as a finger food or appetizer. They are known for their crispy and flavorful exterior and tender meat, making them a crowd-pleasing option for casual gatherings, sports events, or as a delicious snack.",
 	},
 	{
 		id: 14,
@@ -110,6 +136,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
 		price: "$$$$",
+		description:
+			"Baked chicken is a versatile and healthy dish that is popular for its simplicity and flavorful results. It involves cooking chicken pieces or a whole chicken in the oven, resulting in tender and juicy meat with a deliciously seasoned exterior.",
 	},
 	{
 		id: 15,
@@ -118,6 +146,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
 		price: "$",
+		description:
+			"Chicken tenders, also known as chicken fingers or chicken strips, are a popular and versatile dish that consists of boneless and skinless chicken breast meat cut into long, thin strips. They are a beloved choice for both kids and adults due to their tender texture and the ability to customize the flavors ",
 	},
 	{
 		id: 16,
@@ -126,6 +156,8 @@ export const data = [
 		image:
 			"https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
 		price: "$$$",
+		description:
+			"Chicken kabobs, also known as chicken kebabs, are a delicious and versatile dish consisting of skewered pieces of marinated chicken cooked over a grill or in the oven. They are a popular choice for outdoor barbecues, picnics, or as a flavorful main course.  To prepare chicken kabobs, boneless and skinless chicken breast or thigh meat is cut into bite-sized pieces and marinated to infuse it with flavor and tenderness. ",
 	},
 ]
 
