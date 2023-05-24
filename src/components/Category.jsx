1739e7451de14a8a78bbec691bff5696740eb601
Damian Czerwinsky <81160264+Damianchii@users.noreply.github.com>
2023-05-24 22:40:03 +0200

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,7 +4,7 @@ import { categories } from "../data/data"
 export const Category = () => {
 	return (
 		<div className='max-w-[1640px] m-auto px-4 py-12'>
-			<h1 className='text-orange-600 font-bold text-4xl text-center'>
+			<h1 className='text-blue-600 font-bold text-4xl text-center'>
 				Top Rated Menu Items
 			</h1>
 			{/*Categories*/}
