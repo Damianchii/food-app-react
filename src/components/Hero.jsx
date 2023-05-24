8368de82145ededa0ae4386e3e1871d5ba3abc93
Damian Czerwinsky <81160264+Damianchii@users.noreply.github.com>
2023-05-24 22:39:46 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ export const Hero = () => {
 			<div className='max-h-[500px] relative'>
 				{/*Overlay*/}
 				<div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center '>
-					<h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-						The <span className='text-orange-500'>Best</span>
+					<h1 className='px-6 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
+						The <span className='text-blue-500'>Best</span>
 					</h1>
-					<h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-						<span className='text-orange-500'>Foods</span> Delivered
+					<h1 className='px-8 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
+						<span className='text-blue-500'>Foods</span> Delivered
 					</h1>
 				</div>
 				<img
